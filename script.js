279ec35fae6f534875e075039f8828b83635e8c7
Ryan Thorn <ryandthorn@gmail.com>
2018-11-13 14:08:56 -0800

    Squashed commit of the following:
    commit a3887e6ea12789361f6a0c9858c5ebf1cf1535c4
    Author: Ryan Thorn <ryandthorn@gmail.com>
    Date:   Tue Nov 13 14:00:39 2018 -0800
    
        Create source database, implement dynamic endpoint.
    
        getNews() now fetches a URL with query parameters set by user input - it works!
    
        New functions:
        	*formatQueryParams - Encodes query parameters
        	*generateSourceValue - Generates string of IDs from selected sources to
        be used in query params
    
        Decided to structure newsSources as an array of objects with 'id' and 'bias'
        properties. Might like to port to MongoDB at some point.

--- script.js
+++ script.js
@@ -1,9 +1,125 @@
 'use strict';
 
+// Database of ids for News API sources
+const newsSources = [
+  {
+    id: 'abc-news',
+    bias: ['center-left']
+  },
+  {
+    id: 'associated-press',
+    bias: ['center']
+  },
+  {
+    id: 'bbc-news',
+    bias: ['center']
+  },
+  {
+    id: 'bloomberg',
+    bias: ['center']
+  },
+  {
+    id: 'breitbart-news',
+    bias: ['right']
+  },
+  {
+    id: 'cbs-news',
+    bias: ['center-left']
+  },
+  {
+    id: 'cnbc',
+    bias: ['center']
+  },
+  {
+    id: 'cnn',
+    bias: ['left', 'center-left']
+  },
+  {
+    id: 'msnbc',
+    bias: ['left']
+  },
+  {
+    id: 'nbc-news',
+    bias: ['center-left']
+  },
+  {
+    id: 'reuters',
+    bias: ['center']
+  },
+  {
+    id: 'the-american-conservative',
+    bias: ['center-right']
+  },
+  {
+    id: 'the-economist',
+    bias: ['center-left']
+  },
+  {
+    id: 'the-hill',
+    bias: ['center']
+  },
+  {
+    id: 'the-huffington-post',
+    bias: ['left']
+  },
+  {
+    id: 'national-review',
+    bias: ['right']
+  },
+  {
+    id: 'the-new-york-times',
+    bias: ['left', 'center-left']
+  },
+  {
+    id: 'the-wall-street-journal',
+    bias: ['center', 'center-right']
+  },
+  {
+    id: 'the-washington-post',
+    bias: ['center-left']
+  },
+  {
+    id: 'the-washington-times',
+    bias: ['center-right']
+  },
+  {
+    id: 'usa-today',
+    bias: ['center']
+  }
+];
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function generateSourceValue(bias) {
+  return newsSources
+    .filter(source => source.bias.indexOf(bias) > -1)
+    .map(source => source.id)
+    .join(',');
+}
+
 function getNews(searchQuery, sliderVal) {
-  console.log(`Searched for ${searchQuery} & Slider value = ${sliderVal}`);
-  // Hard-coded endpoint for development
-  const url = "https://newsapi.org/v2/everything?q=bitcoin&from=2018-10-13&sortBy=publishedAt";
+  const params = {
+    q: searchQuery,
+  };
+  
+  if (sliderVal === '0') {
+    params.sources = generateSourceValue('left');
+  } else if (sliderVal === '1') {
+    params.sources = generateSourceValue('center-left');
+  } else if (sliderVal === '2') {
+    params.sources = generateSourceValue('center');
+  } else if (sliderVal === '3') {
+    params.sources = generateSourceValue('center-right');
+  } else if (sliderVal === '4'){
+    params.sources = generateSourceValue('right');
+  }
+
+  const queryParams = formatQueryParams(params)
+  const url = `https://newsapi.org/v2/top-headlines?${queryParams}`;
   
   // Exposing API key in source code might be a problem
   const options = {
